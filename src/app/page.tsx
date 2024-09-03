19da02b6ea9b6cee625e71cc6bd4f9f580e4d4a5
Jordan Vera <verawebdevelopment@gmail.com>
2024-09-02 21:50:51 -0500

    chore: Update landing page content and author name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import { Container } from "@/components/Container";
-import { Heading } from "@/components/Heading";
-import { Highlight } from "@/components/Highlight";
-import { Paragraph } from "@/components/Paragraph";
-import { Products } from "@/components/Products";
-import { TechStack } from "@/components/TechStack";
-import Image from "next/image";
+import { Container } from '@/components/Container';
+import { Heading } from '@/components/Heading';
+import { Highlight } from '@/components/Highlight';
+import { Paragraph } from '@/components/Paragraph';
+import { Products } from '@/components/Products';
+import { TechStack } from '@/components/TechStack';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Jordan</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        Hi, I'm Jordan Vera, a passionate software engineer with a knack for
+        creating simple solutions through code. I thrive on the excitement of
+        bringing ideas to life and transforming them into tangible, functional
+        products.
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
+        Beyond coding, I like listening to live music, traveling, and watching
+        football (specifically the Kansas City Chiefs).
       </Paragraph>
       <Heading
         as="h2"
