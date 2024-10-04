a6f9d1aec1ef9ba8e6b5c850137ffb245b0cd1ff
Jordan Vera <verawebdevelopment@gmail.com>
2024-10-03 23:14:43 -0500

    Finished adding links to TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -73,44 +73,46 @@ export const TechStack = () => {
     {
       title: 'Node JS',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653464816/kenjimmy.me/nodejs_ymmm0h.png',
+      link: 'https://nodejs.org/en',
       className: 'h-10 w-10',
     },
     {
       title: 'Express',
       src: '/images/logos/express-colored.svg',
+      link: 'https://expressjs.com/',
       className: 'h-10 w-10 bg-zinc-700 rounded-full p-1',
-    },
-    {
-      title: 'Java',
-      src: '/images/logos/java.png',
-      className: 'h-10 w-10',
     },
     {
       title: 'MongoDB',
       src: '/images/logos/mongodb-colored.svg',
+      link: 'https://www.mongodb.com/',
 
       className: 'h-10 w-10',
     },
     {
       title: 'Mongoose',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653464817/kenjimmy.me/mongoose_fmvf1q.png',
+      link: 'https://mongoosejs.com/',
       className: 'h-12 w-12',
     },
     {
       title: 'MySQL',
       src: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg',
+      link: 'https://www.mysql.com/',
 
       className: 'h-10 w-16',
     },
     {
       title: 'Prisma',
       src: '/images/logos/prisma.png',
+      link: 'https://www.prisma.io/',
 
       className: 'h-10 w-16',
     },
     {
       title: 'Java',
       src: '/images/logos/java.png',
+      link: 'https://www.java.com/en/',
 
       className: 'h-10 w-16',
     },
@@ -118,6 +120,7 @@ export const TechStack = () => {
     {
       title: 'Spring Boot',
       src: '/images/logos/spring-boot.png',
+      link: 'https://spring.io/projects/spring-boot',
 
       className: 'h-10 w-10',
     },
@@ -127,50 +130,50 @@ export const TechStack = () => {
     {
       title: 'npm',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653464815/kenjimmy.me/npm_c4lqfw.png',
-
+      link: 'https://www.npmjs.com/',
       className: 'h-10 w-10',
     },
     {
       title: 'Git',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653464815/kenjimmy.me/git_fmjpe4.png',
-
+      link: 'https://git-scm.com/',
       className: 'h-10 w-10',
     },
 
     {
       title: 'Docker',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653462522/kenjimmy.me/kisspng-using-docker-developing-and-deploying-software-wi-poznaj-aplikacjdocker-drupal-w-15-minut-docke-5b81de1974da70.1534794815352376574786_dcdmie.png',
-
+      link: 'https://www.docker.com/',
       className: 'h-10 w-10',
     },
     {
       title: 'AWS',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653463600/kenjimmy.me/kisspng-logo-amazon-web-services-amazon-com-portable-netwo-5c57904c50a156.9772938415492424443303_rfhji1.png',
-
+      link: 'https://aws.amazon.com/',
       className: 'h-10 w-10',
     },
     {
       title: 'Github',
       src: '/images/logos/github.svg',
-
+      link: 'https://github.com/',
       className: 'h-10 w-10',
     },
     {
       title: 'Heroku',
       src: '/images/logos/heroku-colored.svg',
-
+      link: 'https://www.heroku.com/',
       className: 'h-10 w-10',
     },
     {
       title: 'Webpack',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653464819/kenjimmy.me/webpack_xjsy5q.png',
-
+      link: 'https://webpack.js.org/',
       className: 'h-10 w-10',
     },
     {
       title: 'Postman',
       src: 'https://res.cloudinary.com/dwa1jtluu/image/upload/q_auto,f_auto/v1653464818/kenjimmy.me/postman_thyot5.png',
-
+      link: 'https://www.postman.com/',
       className: 'h-10 w-10',
     },
   ];
@@ -211,6 +214,9 @@ export const TechStack = () => {
           {backEnd.map((item) => (
             <a
               key={item.src}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
               className="hover:scale-110 transition-all duration-300 ease-in-out hover:cursor-pointer"
             >
               <Image
@@ -230,6 +236,9 @@ export const TechStack = () => {
           {others.map((item) => (
             <a
               key={item.src}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
               className="hover:scale-110 transition-all duration-300 ease-in-out hover:cursor-pointer"
             >
               <Image
