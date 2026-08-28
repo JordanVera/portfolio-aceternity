b16d66a8b053a6eb1fb58b440909344adcdb7651
Jordan Vera <verawebdevelopment@gmail.com>
2026-08-28 05:29:40 -0500

    Adjust margin in HomeHero component for improved layout consistency.

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -77,7 +77,7 @@ export const HomeHero = () => {
         </motion.span>
 
         {/* <span className="mt-4 block text-4xl">👋</span> */}
-        <div className="mt-1 flex items-center gap-2">
+        <div className="mt-3 flex items-center gap-2">
           {/* <Heading className="font-black">Hello there! I&apos;m</Heading> */}
           <JordanVeraLogo
             priority
