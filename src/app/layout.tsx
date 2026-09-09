dfed51dc43fbefda091fc95b9a830340e5abc466
Jordan Vera <verawebdevelopment@gmail.com>
2026-09-09 11:47:49 -0500

    Update default theme to 'dark' and adjust layout component to reflect new theme class for improved styling consistency across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="lsu" data-theme="lsu" suppressHydrationWarning>
+    <html lang="en" className="dark" data-theme="dark" suppressHydrationWarning>
       <body
         className={twMerge(
           inter.className,
